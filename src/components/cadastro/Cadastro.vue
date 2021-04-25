b17942fbc9522393d8cf3ba7777e722b2bef3436
Tatiane Mendes Camillo <tatiane@Alexs-MacBook-Pro.local>
2021-04-25 18:11:32 -0300

    form created into cadastro.vue, used v-model for two way binding

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,15 +1,88 @@
 <template>
    <div>
     <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <!-- @input="foto.titulo = $event.target.value" :value="foto.titulo" = v.model-->
+        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo">
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        <imagem-responsiva v-show="foto.url" :titulo="foto.titulo" :url="foto.url"/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" v-model="foto.descricao"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link to="/"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
   </div>
 </template>
-<script>
-export default {
 
+<script>
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+
+export default {
+  components: {
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
+  },
+  data() {
+    return {
+      foto: {
+        titulo: '',
+        url: '',
+        descricao: ''
+      }
+    }
+  },
+  methods: {
+    grava(){
+      console.log(this.foto);
+      this.foto = {
+          titulo: '',
+          url: '',
+          descricao: ''
+      };
+    }
+  }
 }
 </script>
+
 <style scoped>
-   .centralizado {
-        text-align: center;
-    }
+  .centralizado {
+    text-align: center;
+  }
+
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
 </style>
